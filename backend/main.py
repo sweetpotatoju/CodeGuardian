5fd16b214214149c607912a33e93c4c886328a12
sweetpotatoju <jeju3146@naver.com>
2025-03-11 17:11:47 +0900

    add DB connect/mainpage

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
+from backend.DB.base import SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -12,10 +23,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/check-db")
+def check_db_connection(db: Session = Depends(get_db)):  # ✅ get_db() 사용
+    try:
+        result = db.execute(text("SELECT version();"))
+        version = result.fetchone()
+        return {"status": "success", "postgresql_version": version[0]}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
